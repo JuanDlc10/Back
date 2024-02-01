b36d415f28ccf7ba7303588a81e37b666602c842
Juan Dlc <daner_10@live.com.mx>
2024-02-01 14:12:54 -0600

    v2

--- index.php
+++ index.php
@@ -6,6 +6,18 @@ $dotenv = Dotenv\Dotenv::createImmutable('./');
 $dotenv -> load();
 
 
-echo $_ENV['MI_VARIABLE_ENTORNO']
+$host = $_ENV['HOST'];
+$port = $_ENV['PORT'];
+$database = $_ENV['DB'];
+$username = $_ENV['USER'];
+$password = $_ENV['PASSWORD'];
+
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
+    echo "Nos conectamos a la nasa :)";
+} catch (PDOException $e) {
+    echo "No nos pudimos conectar a la nasa :(";
+}
+
 
 ?>
