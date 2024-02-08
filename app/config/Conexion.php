9a949dbf40385694932cb8f1c61c10f87639dd28
Juan Dlc <daner_10@live.com.mx>
2024-02-08 11:45:51 -0600

    Tarea

--- app/config/Conexion.php
+++ app/config/Conexion.php
@@ -41,18 +41,61 @@ class Conexion
     {
         self::$conexion = null;
     }
+}
+
+
+class Crud
+{
     public static function mostrar_datos()
     {
-        $consulta = Conexion::obtener_conexion()->prepare("select * from t_prueba");
-        if (!$consulta -> execute()){
+        $consulta = Conexion::obtener_conexion()->prepare("SELECT * FROM t_prueba");
+        if (!$consulta->execute()) {
             echo 'No se pudo realizar la consulta';
         } else {
-            $dato = $consulta->fetchAll (PDO::FETCH_ASSOC);
+            $dato = $consulta->fetchAll(PDO::FETCH_ASSOC);
             echo print_r($dato);
             echo 'Se completo la peticion';
         }
     }
-}
+    public static function insertar_datos()
+    {
+        $peticion = Conexion::obtener_conexion()->prepare("INSERT INTO t_prueba (nombre) VALUES ('Alejandro')");
+        if (!$peticion->execute()) {
+            echo 'No se pudo realizar la inserción';
+        } else {
+            echo 'Se completó la inserción de datos';
+        }
+    }
+    public static function editar_datos()
+    {
+        $id = 5;
+        $nuevo_nombre = 'Pamfilo';
 
-Conexion::mostrar_datos();
+        $peticion = Conexion::obtener_conexion()->prepare("UPDATE t_prueba SET nombre = :nuevo_nombre WHERE id = :id");
+        $peticion->bindParam(':nuevo_nombre', $nuevo_nombre, PDO::PARAM_STR);
+        $peticion->bindParam(':id', $id, PDO::PARAM_INT);
+
+        if (!$peticion->execute()) {
+            echo 'No se pudo realizar la actualización';
+        } else {
+            echo 'Se completó la actualización de datos';
+        }
+    }
+    public static function eliminar_datos()
+    {
+        $id = 5;
+        $peticion = Conexion::obtener_conexion()->prepare("DELETE FROM t_prueba WHERE id = :id");
+        $peticion->bindParam(':id', $id, PDO::PARAM_INT);
+
+        if (!$peticion->execute()) {
+            echo 'No se pudo realizar la eliminación';
+        } else {
+            echo 'Se completó la eliminación de datos';
+        }
+    }
+}
+/* Crud::insertar_datos(); */
+/* Crud::editar_datos(); */
+/* Crud::eliminar_datos(); */
+Crud::mostrar_datos();
 ?>
